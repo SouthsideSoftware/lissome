5e1dc40acc3c50aa0dc7f0c978faecb590b8701a
Tom Cabanski <tom@cabanski.com>
2013-10-05 19:21:30 -0500

    HTML 5 boilerplate for site.

--- backend/node.js/routes/index.js
+++ backend/node.js/routes/index.js
@@ -1,3 +1,9 @@
 exports.attachHandlers = function attachHandlers(app){
     require('./api')(app);
+
+    app.get('/', function(req, res){
+        res.render('index',
+            { title : 'Home' }
+        )
+    })
 };
